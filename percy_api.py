86ee1152996044e91e4d2e85f23ae9efdbce3ee2
Ansel Francis Dsouza <ansel20@gmail.com>
2022-11-07 22:46:52 +0530

    Create percy_api.py

---
+++ percy_api.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import sys
+import os
+
+url = "https://percy.io/api/v1/projects?project_slug=%s"%(sys.argv[1])
+
+headers = {"Authorization": "Token %s"%(os.environ['PERCY_TOKEN'])}
+response = requests.get(url, headers=headers)
+response = response.json()
+project_id = response["data"]["id"]
+
+url = "https://percy.io/api/v1/builds?project_id=%s"%(project_id)
+
+response = requests.get(url, headers=headers)
+response = response.json()
+build_id = response['data'][0]['id']
+
+url = "https://percy.io/api/v1/builds/%s"%(build_id)
+
+response = requests.get(url, headers=headers)
+response = response.json()
+build_state = response['data']['attributes']['state']
+build_review_state = response['data']['attributes']['review-state']
+build_review_state_reason = response['data']['attributes']['review-state-reason']
+print("Below is the status of the build %s\n"%(build_id))
+print("state: ",build_state)
+print("review-state: ",build_review_state)
+print("review-state-reason: ",build_review_state_reason)
+print("\n====================================================\n")
+
+url = "https://percy.io/api/v1/snapshots?build_id=%s"%(build_id)
+response = requests.get(url, headers=headers)
+response = response.json()
+print("\nBelow is the status of the snapshot for build %s\n"%(build_id))
+for snapshot in response['data']:
+    snapshot_name = snapshot['attributes']['name']
+    snapshot_review_state = snapshot['attributes']['review-state']
+    snapshot_review_state_reason = snapshot['attributes']['review-state-reason']
+    if snapshot_review_state != "approved":
+        print("name: ",snapshot_name)
+        print("review-state: ",snapshot_review_state)
+        print("review-state-reason: ",snapshot_review_state_reason)
+        print('\n')
